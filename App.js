43929638e5f1c26cf4ac35b93928842d7acaf06a
RayhaanSahuKhanTAFENSW <rayhaan.sahukhan@studytafensw.edu.au>
2024-11-11 12:13:11 +1100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RootNavigator from './navigation/RootNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+    
   );
 }
 
